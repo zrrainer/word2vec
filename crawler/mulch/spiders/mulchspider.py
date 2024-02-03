255e498d3aa40bdbaae3a3c7b9044a75b3c04ca1
Rainer <zrrainer@gmail.com>
2024-02-03 14:30:03 +1100

    2024.2.3

--- crawler/mulch/spiders/mulchspider.py
+++ crawler/mulch/spiders/mulchspider.py
@@ -1,13 +1,15 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
+import re
 
 class MulchSpider(scrapy.Spider):
     name = "mulch"
-    urls = ["https://www.bloodborne-wiki.com/p/story.html",
-            "http://www.bloodborne-wiki.com/p/index.html"]
+    urls = ["https://www.bloodborne-wiki.com/p/story.html"]
     count = 0
     link_extractor = LinkExtractor()
     log = "" #tesing purposes
+    text_out = ""
+
 
 
     def start_requests(self):
@@ -23,7 +25,6 @@ class MulchSpider(scrapy.Spider):
     def parse(self, response):
         texts = response.xpath("//body//text()").getall()
         links = self.link_extractor.extract_links(response)
-        text_out = ""
         links_out = ""
 
 
@@ -31,12 +32,16 @@ class MulchSpider(scrapy.Spider):
 
 
 
-        #define how to output the text
-        # why isnt this working
-        #ITS WORKING
-        text_out = "".join(texts)
-        f = open("output.txt", "w", encoding="utf-8")
-        f.write(text_out)
+        #record content ofcurrent node
+
+
+        #define how to filter text
+        texts = MulchSpider.cleanstring(texts)
+        texts = filter(lambda text: "maintenance" not in text, texts)
+        self.text_out = self.text_out + "\n".join(texts)
+
+        
+    
 
 
         #search algortithm
@@ -55,23 +60,47 @@ class MulchSpider(scrapy.Spider):
         self.log = self.log + "frontier lengths" + str(len(self.urls)) + "\n" ##
         if len(self.urls) == 0:
             self.log = self.log + "search finished \n"
+            f = open("output.txt", "w", encoding="utf-8")
+            f.write(self.text_out)
         else:
             self.urls = self.urls[1:]
             self.log = self.log + "removing:" + self.urls[0] + "\n\n" ##
 
             #hault operation after scraping a given number of sites
-            if self.count > 5:
+            if self.count > 10:
+                f = open("output.txt", "w", encoding="utf-8")
+                f.write(self.text_out)
                 return
             yield scrapy.Request(url=self.urls[0], callback=self.parse)
             self.log = self.log + "test yielding request: " + self.urls[0] + "\n"
+            self.text_out = self.text_out + "----------------------------new request----------------------------\n"
             self.count = self.count + 1
             self.log = self.log + "count: " + str(self.count) + "\n"
-        
 
 
-
-
+        f = open("output.txt", "w") #this is not writing
+        f.write(self.text_out)
 
         g = open("log.txt", "w")
         g.write(self.log)
 
+
+
+
+    #param: a list of strings
+    #output: same list of string, normalized. 
+    @classmethod
+    def cleanstring(cls, strings):
+        #format string properly
+        
+        #remove all \n, special characters, lowercase all
+        new_strings = []
+        for string in strings:
+
+            string = string.replace('\n', ' ')
+            string=re.sub(r"\W+|_", " ", string) 
+            string = string.lower()
+            if any(character.isalnum() for character in string): #if string contain alphanumericals
+                new_strings.append(string)
+
+        return new_strings
