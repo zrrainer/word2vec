ad345127831574dabcc2547cd8d64037c3627ea5
Rainer <zrrainer@gmail.com>
2024-02-08 11:07:03 +1100

    crawler implemented (YAY)

--- crawler/mulch/spiders/mulchspider.py
+++ crawler/mulch/spiders/mulchspider.py
@@ -1,6 +1,7 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 import re
+import csv
 
 class MulchSpider(scrapy.Spider):
     name = "mulch"
@@ -8,47 +9,46 @@ class MulchSpider(scrapy.Spider):
             "https://www.bloodborne-wiki.com/p/firearms.html"]
     count = 0
     link_extractor = LinkExtractor()
+    explored_links = []
     text_out = ""
     #implement visited links
 
 
 
     def start_requests(self):
-        for url in self.urls:
-            yield scrapy.Request(url=url, callback=self.parse)    
- 
+        url = self.urls[0]
+
+        if url not in self.explored_links:
+            yield scrapy.Request(url=url, callback=self.parse)
+            self.count += 1
+
+        self.urls = self.urls[1:] 
+
 
     def parse(self, response):
         texts = response.xpath("//body//text()").getall()
         links = self.link_extractor.extract_links(response)
-        links_out = ""
-
 
         #record content of current node
         texts = MulchSpider.cleanString(texts)
         #texts = filter(lambda text: "vergil" in text, texts)  #define how to filter text
         self.text_out = self.text_out + "\n".join(texts)
 
-        #search algortithm
-        explored_links = []
-
         #explore current node
         for link in links:
             if link.url not in self.urls:
                 self.urls.append(link.url)  
-        explored_links.append(self.urls[0])
+        self.explored_links.append(self.urls[0])
 
+        #next node
+        while (len(self.urls) > 0 and self.count < 10):
+            yield from self.start_requests() 
 
-            
-        if (len(self.urls) > 0 and self.count < 10):
-            self.text_out = self.text_out + "\n\n\n ----------------------------------------\nif condition met"
-            self.start_requests() #debugging.................
-            # self.urls = self.urls[1:]
-            # self.count = self.count + 1
-            # yield scrapy.Request(url=self.urls[0], callback=self.parse)
-
+        #writing output
         f = open("output.txt", "w", encoding="utf-8")
         f.write(self.text_out)
+
+        self.writeCSV(self.explored_links)
 
 
 
@@ -59,10 +59,9 @@ class MulchSpider(scrapy.Spider):
     def cleanString(cls, strings):
         #format string properly
         
-        #remove all \n, special characters, lowercase all
+        #remove all extra line breaks, special characters, lowercase all
         new_strings = []
         for string in strings:
-
             string = string.replace('\n', ' ')
             string = re.sub(r"\W+|_", " ", string) 
             string = string.lower()
@@ -72,11 +71,19 @@ class MulchSpider(scrapy.Spider):
         return new_strings
     
 
-    #def yield request
-        #remove current node
-        #yielf request 
-        #count ++
-    def yieldRequest(self):
-        self.urls = self.urls[1:]
-        self.count = self.count + 1
-        yield scrapy.Request(url=self.urls[0], callback=self.parse) #this is the problem everything else is reached
+    @classmethod
+    def writeCSV(cls, list):
+        with open('CSVtest.csv', 'w', encoding="utf-8") as CSVtest:
+            wr = csv.writer(CSVtest, quoting=csv.QUOTE_NONE)
+            wr.writerow(list)
+
+    #returns list
+    @classmethod
+    def readCSV(cls, filepath):
+        list = []
+        with open(filepath, newline='') as csvfile:
+            reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+            for row in reader:
+                list.append(row)
+
+        return list
---
+++ crawler/mulch/spiders/test.py
@@ -0,0 +1,4 @@
+from mulchspider import MulchSpider
+
+list = MulchSpider.readCSV("crawler\CSVtest.csv") #no such file
+print(list)
